ab6b382c52803424bc77b37a8ab081e2ba618d9b
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-02 12:50:43 +0200

    Added login, sign up page layouts

--- resources/js/Components/Layouts/GuestLayout.jsx
+++ resources/js/Components/Layouts/GuestLayout.jsx
@@ -11,7 +11,6 @@ export default function GuestLayout(){
 
 return(
    <div>
-       <div>For guests users...</div>
        <Outlet/>
    </div>
 )
--- resources/js/Pages/Signup.jsx
+++ resources/js/Pages/Signup.jsx
@@ -1,7 +1,26 @@
+import {Link} from "react-router-dom";
+
 export default function Signup(){
-return(
-    <>
-        Signup
-    </>
-)
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+    }
+
+    return(
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Register new account</h1>
+                    <input placeholder="Name" type="text"/>
+                    <input placeholder="Email" type="text"/>
+                    <input placeholder="Password" type="password"/>
+                    <input placeholder="Password confirmation" type="password_confirmation"/>
+                    <button className="btn btn-block">Sign up</button>
+                    <p className="message">
+                       Already have an account? <Link to="/login">Sign in</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    )
 }
